3e676f24920ccc36e77476be1f332266f6264b9a
David Javier Zegarra Flórez <d.zegarra@uniandes.edu.co>
2023-11-09 23:10:14 -0500

    explorando datos 1

--- scripts/01_data_cleaning.R
+++ scripts/01_data_cleaning.R
@@ -1,8 +1,54 @@
 # 1| Importar -------------------------------------------------------------
 # Importamos la base de datos de entrenamiento y de evaluación.
-dataset_hog <- read.csv(file = paste0(directorioDatos, 'train_hogares.csv'))
-dataset_kaggle_hog <- read.csv(file = paste0(directorioDatos,'test_hogares.csv'))
+data_hog <- read.csv(file = paste0(directorioDatos, 'train_hogares.csv'))
+data_kaggle_hog <- read.csv(file = paste0(directorioDatos,'test_hogares.csv'))
 
-dataset_p <- read.csv(file = paste0(directorioDatos, 'train_personas.csv'))
-dataset_kaggle_p <- read.csv(file = paste0(directorioDatos,'test_personas.csv'))
+data_p <- read.csv(file = paste0(directorioDatos, 'train_personas.csv'))
+data_kaggle_p <- read.csv(file = paste0(directorioDatos,'test_personas.csv'))
+
+summary(data_hog$Ingpcug)
+
+# 33024 hogares pobres 
+table(data_hog$Pobre)
+
+table(data_hog$Npobres)
+table(data_hog$Depto)
+
+
+# Factor de expansión departamental
+table(data_hog$Fex_dpto)
+
+table(data_hog$Clase)
+table(data_hog$Dominio)
+
+
+#----------------------------------------------------------------------------
+# Variables que podriamos extraer de Personas
+# Estrato1 , 
+
+#----------------------------------------------------------------------------
+# Variables que estan en train y no test
+# HOGAR
+names(data_hog)
+names(data_kaggle_hog)
+diff_vars_hog <- setdiff(names(data_hog), names(data_kaggle_hog))
+diff_vars_hog
+# "Ingtotug"    "Ingtotugarr" "Ingpcug"  "Pobre"       "Indigente"   "Npobres"     "Nindigentes"
+
+
+
+# PERSONA
+names(data_p)
+names(data_kaggle_p)
+#conflicted::conflicts_prefer(base::setdiff)
+diff_vars_persona <- setdiff(names(data_p), names(data_kaggle_p))
+diff_vars_persona
+
+# Basicamente se borraron todas las variables cuantitativas de algún tipo de ingreso.
+
+#----------------------------------------------------------------------------
+
+
+dataset_merged <- left_join(dataset_hog, dataset_p, by = c("id", "Clase", "Dominio") )
+
 
